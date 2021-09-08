9bc29bfbbb87a25ae4735368db2d229675718c22
Tobias Kunze <r@rixx.de>
2021-09-09 00:25:07 +0200

    Hide pretalx-mozilla locale if inactive

--- pretalx_mozilla/signals.py
+++ pretalx_mozilla/signals.py
@@ -5,4 +5,8 @@ from pretalx.common.signals import register_locales
 
 @receiver(register_locales)
 def register_locales(**kwargs):
+    event = kwargs.pop("sender", None)
+    if event:
+        if "pretalx_mozilla" not in getattr(event, "plugin_list") or []:
+            return []
     return ["en-mozilla"]
